988241c520334bcb1d5e31008ba84ad43976cf2f
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-09-14 17:27:47 +0200

    custom token

---
+++ ProductivityTools.Transfers.Api/Controllers/CustomTokenController.cs
@@ -0,0 +1,17 @@
+﻿using FirebaseAdmin.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductivityTools.Transfers.WebApi.Controllers
+{
+    public class CustomTokenController : Controller
+    {
+        [HttpGet]
+        [Route("GetPython")]
+        public async Task<string> GetToken()
+        {
+            var uid = "GetPython";
+            string customToken = await FirebaseAuth.DefaultInstance.CreateCustomTokenAsync(uid);
+            return customToken;
+        }
+    }
+}
--- ProductivityTools.Transfers.Api/Program.cs
+++ ProductivityTools.Transfers.Api/Program.cs
@@ -1,8 +1,37 @@
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.IdentityModel.Logging;
 using ProductivityTools.Transfers.Database;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens; 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+string masterconfpath = Environment.GetEnvironmentVariable("MasterConfigurationPath");
+FirebaseApp.Create(new AppOptions()
+{
+    Credential = GoogleCredential.FromFile($"{masterconfpath}\\ProductivityTools.Transfers.ServiceAccount.json"),
+});
+builder.Services
+ .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+ .AddJwtBearer(options =>
+ {
+     options.Authority = "https://securetoken.google.com/pttransfersprod";
+     options.TokenValidationParameters = new TokenValidationParameters
+     {
+         ValidateIssuer = true,
+         ValidIssuer = "https://securetoken.google.com/pttransfersprod",
+         ValidateAudience = true,
+         ValidAudience = "pttransfersprod",
+         ValidateLifetime = true
+     };
+ });
+
+IdentityModelEventSource.ShowPII = true;
+
+
 
 builder.Services.AddControllers();
 builder.Services.ConfigureServicesDatabase();
