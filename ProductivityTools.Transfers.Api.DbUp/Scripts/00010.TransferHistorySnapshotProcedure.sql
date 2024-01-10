95f0c365b6fbf6ee2e308dbc3ce31dae6c6d99b3
Pawel Wujczyk <pwujczyk@hotmail.com>
2024-01-10 21:40:18 +0100

    stored procedure

---
+++ ProductivityTools.Transfers.Api.DbUp/Scripts/00010.TransferHistorySnapshotProcedure.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE dbo.TodayTransferHistory
+AS
+DECLARE @Today DATE
+SELECT top 1 @Today=[Date] FROM [PTTransfers].[dbo].[TransferHistorySnapshot]
+SELECT @Today
+
+DELETE [dbo].[TransferHistory] WHERE Date=@Today
+INSERT INTO [dbo].[TransferHistory] (Date,Source,Target,Value,ValueComment)
+Select Date,Source,Target,Value,ValueComment FROM dbo.TransferHistorySnapshot
